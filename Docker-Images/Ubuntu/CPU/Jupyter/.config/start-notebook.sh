d21d61b2be6e5c371aeaeb03180610a7447f8c0b
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-10-30 15:03:40 -0300

    Update start-notebook.sh

--- Docker-Images/Ubuntu/CPU/Jupyter/.config/start-notebook.sh
+++ Docker-Images/Ubuntu/CPU/Jupyter/.config/start-notebook.sh
@@ -5,3 +5,11 @@
 set -e
 
 jupyter-$JUPYTER_TYPE --ip=0.0.0.0 --port=$JUPYTER_PORT --no-browser --notebook-dir=$HOME/workdir/notebooks --allow-root
+
+echo " " >> /.config/.login
+echo " " >> /.config/.login
+echo "############## Jupyter Notebook ##############" >> /.config/.login
+echo " " >> /.config/.login
+jupyter noteebook list >> /.config/.login
+echo " "
+cat /.config/.login
