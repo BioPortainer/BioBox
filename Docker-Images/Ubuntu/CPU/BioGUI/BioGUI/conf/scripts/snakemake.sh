23ed85710bc66b61f12217caec57adc80e71041d
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-12-05 14:31:51 -0200

    Delete snakemake.sh

--- Docker-Images/Ubuntu/CPU/BioGUI/BioGUI/conf/scripts/snakemake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Extracting GreenGenes Database..."
-gunzip -c ./additional_files/97_otus.fasta.gz > ./additional_files/
-echo "Pipeline Starting..."
-echo " "
-echo " "
-snakemake $@
